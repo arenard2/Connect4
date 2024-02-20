ee47ea13b4a8e3ec3948b2aa1cd9b4d6384a0c1b
Anatole Renard <anatole.renard@mrjaffesclass.com>
2024-02-20 12:38:51 -0800

    board

---
+++ Board.java
@@ -0,0 +1,17 @@
+public class Board
+{
+    private char board[][];
+    
+    public Board()
+    {
+        board = new char[6][7];
+    }
+    
+    /*Returns 1 if move is possible and 0 if not*/
+    public int makeMove(int pos)
+    {
+        return 0;
+    }
+    
+    
+}
---
+++ Player.java
@@ -0,0 +1,12 @@
+public class Player
+{
+    
+    private char name;
+    
+    public Player(char _name)
+    {
+        name = _name;
+    }
+    
+    public char getName(){return name;}
+}
